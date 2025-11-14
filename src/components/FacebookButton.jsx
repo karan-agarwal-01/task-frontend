82fe0350964ad943849a83a888d540e557a8dd6c
karan-agarwal-01 <karanagarwal858@gmail.com>
2025-11-14 23:21:19 +0530

    new changes

--- src/components/FacebookButton.jsx
+++ src/components/FacebookButton.jsx
@@ -14,54 +14,73 @@ export default function FB() {
     };
 
     return (
-        // <FacebookLogin
-        //   appId="1186947169471468"
-        //   onSuccess={responseFacebook}
-        //   onFail={(error) => console.log(error)}
-        //   onProfileSuccess={(response) => console.log(response)}
-        //   render={({ onClick }) => (
-        //     <div
-        //       onClick={onClick}
-        //       className="cursor-pointer w-full flex items-center justify-center gap-2 border border-gray-300 py-2 my-2 rounded-lg hover:bg-gray-100 transition"
-        //     >
-        //       <img
+        // // <FacebookLogin
+        // //   appId="1186947169471468"
+        // //   onSuccess={responseFacebook}
+        // //   onFail={(error) => console.log(error)}
+        // //   onProfileSuccess={(response) => console.log(response)}
+        // //   render={({ onClick }) => (
+        // //     <div
+        // //       onClick={onClick}
+        // //       className="cursor-pointer w-full flex items-center justify-center gap-2 border border-gray-300 py-2 my-2 rounded-lg hover:bg-gray-100 transition"
+        // //     >
+        // //       <img
+        // //         src="https://www.svgrepo.com/show/475647/facebook-color.svg"
+        // //         alt="facebook"
+        // //         className="w-5 h-5"
+        // //       />
+        // //       <span>Continue with Facebook</span>
+        // //     </div>
+        // //   )}
+        // // />
+        // <div  className="cursor-pointer w-full flex items-center justify-center gap-2 border border-gray-300 py-2 my-2 rounded-lg hover:bg-gray-100 transition">
+        //      <img
         //         src="https://www.svgrepo.com/show/475647/facebook-color.svg"
         //         alt="facebook"
         //         className="w-5 h-5"
         //       />
-        //       <span>Continue with Facebook</span>
-        //     </div>
-        //   )}
-        // />
-        <div  className="cursor-pointer w-full flex items-center justify-center gap-2 border border-gray-300 py-2 my-2 rounded-lg hover:bg-gray-100 transition">
-             <img
+        //     <FacebookLogin
+        //         appId={870742708859759}
+        //         autoLoad={false}
+        //         fields='name,email,picture'
+        //         callback={responseFacebook}
+        //     />
+        // </div>
+        // // render={(renderProps) => (
+        // //     // <button
+        // //     //     onClick={renderProps.onClick}
+        // //     //     className="flex items-center justify-center px-4 py-2 border border-gray-300 rounded-lg hover:bg-gray-50"
+        // //     // >
+        // //     //     {/* <FaFacebookF className="w-5 h-5 mr-2" /> */}
+        // //     //     <span className="text-sm font-medium">Facebook</span>
+        // //     // </button>
+        // //     <Button
+        // //     type="primary"
+        // //     icon={<FacebookOutlined />}
+        // //     onClick={renderProps.onClick}
+        // //     className="bg-blue-600 hover:bg-blue-700 border-blue-600"
+        // // >
+        // //     Login With Facebook
+        // // </Button>
+        // // )}
+        <FacebookLogin
+          appId="1186947169471468"
+          onSuccess={responseFacebook}
+          onFail={(error) => console.log(error)}
+          onProfileSuccess={(response) => console.log(response)}
+          render={({ onClick }) => (
+            <div
+              onClick={onClick}
+              className="cursor-pointer w-full flex items-center justify-center gap-2 border border-gray-300 py-2 my-2 rounded-lg hover:bg-gray-100 transition"
+            >
+              <img
                 src="https://www.svgrepo.com/show/475647/facebook-color.svg"
                 alt="facebook"
                 className="w-5 h-5"
               />
-            <FacebookLogin
-                appId={1186947169471468}
-                autoLoad={false}
-                fields='name,email,picture'
-                callback={responseFacebook}
-            />
-        </div>
-        // render={(renderProps) => (
-        //     // <button
-        //     //     onClick={renderProps.onClick}
-        //     //     className="flex items-center justify-center px-4 py-2 border border-gray-300 rounded-lg hover:bg-gray-50"
-        //     // >
-        //     //     {/* <FaFacebookF className="w-5 h-5 mr-2" /> */}
-        //     //     <span className="text-sm font-medium">Facebook</span>
-        //     // </button>
-        //     <Button
-        //     type="primary"
-        //     icon={<FacebookOutlined />}
-        //     onClick={renderProps.onClick}
-        //     className="bg-blue-600 hover:bg-blue-700 border-blue-600"
-        // >
-        //     Login With Facebook
-        // </Button>
-        // )}
+              <span>Continue with Facebook</span>
+            </div>
+          )}
+        />
     );
 }
