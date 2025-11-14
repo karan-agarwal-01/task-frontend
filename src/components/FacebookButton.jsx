3d71b9c68c69b1b5367beb37f8eb6bc4a8b9ad5f
karan-agarwal-01 <karanagarwal858@gmail.com>
2025-11-14 21:02:13 +0530

    new changes

--- src/components/FacebookButton.jsx
+++ src/components/FacebookButton.jsx
@@ -1,10 +1,9 @@
-import FacebookLogin from "react-facebook-login";
+import FacebookLogin from "@greatsumini/react-facebook-login";
 
 export default function FB() {
   const responseFacebook = (response) => {
     console.log(response);
 
-    // Send token to backend
     fetch("https://task-backend-eight-delta.vercel.app/api/facebook-login", {
       method: "POST",
       headers: {
@@ -15,9 +14,24 @@ export default function FB() {
   };
 
   return (
-    <div className="cursor-pointer w-full flex items-center justify-center gap-2 border border-gray-300 py-2 my-2 rounded-lg hover:bg-gray-100 transition">
-        <img src="https://www.svgrepo.com/show/475647/facebook-color.svg" alt="facebook" className="w-5 h-5" />  
-        <FacebookLogin appId="1225272112996282" autoLoad={false} fields="name,email,picture" callback={responseFacebook} cssClass="cursor-pointer" />
-    </div>
+    <FacebookLogin
+      appId="1225272112996282"
+      onSuccess={responseFacebook}
+      onFail={(error) => console.log(error)}
+      onProfileSuccess={(response) => console.log(response)}
+      render={({ onClick }) => (
+        <div
+          onClick={onClick}
+          className="cursor-pointer w-full flex items-center justify-center gap-2 border border-gray-300 py-2 my-2 rounded-lg hover:bg-gray-100 transition"
+        >
+          <img
+            src="https://www.svgrepo.com/show/475647/facebook-color.svg"
+            alt="facebook"
+            className="w-5 h-5"
+          />
+          <span>Continue with Facebook</span>
+        </div>
+      )}
+    />
   );
 }
